28f1f58b229be472c828aa6d71f9eec6e40d6520
Matthew Pfister <herrpfister@gmail.com>
2020-03-07 17:32:23 -0500

    Update getting_started.js

--- getting_started.js
+++ getting_started.js
@@ -16,10 +16,38 @@ async function runCommand(cmd) {
 }
 
 async function pullRepositories() {
-  let passing = true;
-  passing = await runCommand('mkdir mtglm');
-  passing = await runCommand('cd mtglm && git clone [repo]');
-  // Todo: rest of commands go here
+  const promises = [];
+
+  promises.push(runCommand('mkdir mtglm'));
+  promises.push(runCommand('cd mtglm && git clone [repo]'));
+  promises.push(runCommand('cd mtglm && git clone [repo]'));
+  promises.push(runCommand('cd mtglm && git clone [repo]'));
+  promises.push(runCommand('cd mtglm && git clone [repo]'));
+  promises.push(runCommand('cd mtglm && git clone [repo]'));
+  promises.push(runCommand('cd mtglm && git clone [repo]'));
+  promises.push(runCommand('cd mtglm && git clone [repo]'));
+
+  const passing = await Promise.all(promises)
+
+  return passing.every(pass => pass);
 }
 
 pullRepositories();
+
+async function buildRepositories() {
+  const promises = [];
+
+  promises.push(runCommand('cd mtglm/[repo] && yarn build'));
+  promises.push(runCommand('cd mtglm/[repo] && yarn build'));
+  promises.push(runCommand('cd mtglm/[repo] && yarn build'));
+  promises.push(runCommand('cd mtglm/[repo] && yarn build'));
+  promises.push(runCommand('cd mtglm/[repo] && yarn build'));
+  promises.push(runCommand('cd mtglm/[repo] && yarn build'));
+  promises.push(runCommand('cd mtglm/[repo] && yarn build'));
+
+  const passing = await Promise.all(promises)
+
+  return passing.every(pass => pass);
+}
+
+buildRepositories();
